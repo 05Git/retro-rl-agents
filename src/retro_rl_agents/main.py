f0bf718529bf03a73a7964a8a4dba1f4efec0235
Oscar Sellors <omsellors98@gmail.com>
2026-02-28 18:29:42 +0000

    Specify retro imports

--- src/retro_rl_agents/main.py
+++ src/retro_rl_agents/main.py
@@ -1,7 +1,7 @@
-import stable_retro as retro
 import yaml
 
 from pathlib import Path
+from stable_retro import RetroEnv, make
 
 from retro_rl_agents.cli.arguments import get_args
 from retro_rl_agents.utils.constants import GAME_NAME_MAP
@@ -9,11 +9,11 @@ from retro_rl_agents.rl_models.load import load_model
 from retro_rl_agents.services.call import call_service
 from retro_rl_agents.data_models.config_data import ConfigData
 
-def make_env(env: str) -> retro.RetroEnv:
+def make_env(env: str) -> RetroEnv:
     try:
-        return retro.make(env)
+        return make(env)
     except FileNotFoundError:
-        return retro.make(GAME_NAME_MAP[env])
+        return make(GAME_NAME_MAP[env])
     
 def load_config(config_path: Path) -> ConfigData:
     if not config_path.is_file():
@@ -31,15 +31,17 @@ def load_config(config_path: Path) -> ConfigData:
     return ConfigData(config_path=config_path, **data)
 
 def main():
-    args  = get_args()
+    args = get_args()
 
     game_name = args.game
     env = make_env(game_name)
     
     service_type = args.service
     config_path = args.config_path
+
     if config_path is None:
         config_path = f"configs/{game_name}/{service_type}.yml"
+
     resolved_config_path = Path.cwd().resolve() / config_path
     config = load_config(config_path=resolved_config_path)
     
