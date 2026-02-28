282cb02d11cb55cc932ad33d308ec9eceaeb63ee
Oscar Sellors <omsellors98@gmail.com>
2026-02-28 16:54:13 +0000

    Useful constants and exceptions (exceptions debateable, will flesh out after adding logging)

---
+++ src/retro_rl_agents/utils/constants.py
@@ -0,0 +1,9 @@
+import torch as th
+
+DEVICE = th.device("cuda" if th.cuda.is_available() else "cpu")
+GAME_NAME_MAP: dict[str, str] = {
+    "sf2": "StreetFighterIISpecialChampionEdition-Genesis-v0"
+}
+VALID_SERVICES: list[str] = [
+    "train"
+]
