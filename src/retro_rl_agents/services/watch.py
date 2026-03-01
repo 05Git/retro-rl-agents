69b5fc0e193fb31f010c1240a824df2a7af78408
Oscar Sellors <omsellors98@gmail.com>
2026-03-01 16:08:02 +0000

    Add watch service - Should add more debug info later. - Currently throwing errs because SF2's action space is MultiBinary. Look into this later. Traceback: ``` Traceback (most recent call last):   File "/usr/lib/python3.12/runpy.py", line 198, in _run_module_as_main     return _run_code(code, main_globals, None,            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/usr/lib/python3.12/runpy.py", line 88, in _run_code     exec(code, run_globals)   File "/home/oscar/.vscode/extensions/ms-python.debugpy-2025.18.0-linux-x64/bundled/libs/debugpy/adapter/../../debugpy/launcher/../../debugpy/__main__.py", line 71, in <module>     cli.main()   File "/home/oscar/.vscode/extensions/ms-python.debugpy-2025.18.0-linux-x64/bundled/libs/debugpy/adapter/../../debugpy/launcher/../../debugpy/../debugpy/server/cli.py", line 508, in main     run()   File "/home/oscar/.vscode/extensions/ms-python.debugpy-2025.18.0-linux-x64/bundled/libs/debugpy/adapter/../../debugpy/launcher/../../debugpy/../debugpy/server/cli.py", line 358, in run_file     runpy.run_path(target, run_name="__main__")   File "/home/oscar/.vscode/extensions/ms-python.debugpy-2025.18.0-linux-x64/bundled/libs/debugpy/_vendored/pydevd/_pydevd_bundle/pydevd_runpy.py", line 310, in run_path     return _run_module_code(code, init_globals, run_name, pkg_name=pkg_name, script_name=fname)            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/home/oscar/.vscode/extensions/ms-python.debugpy-2025.18.0-linux-x64/bundled/libs/debugpy/_vendored/pydevd/_pydevd_bundle/pydevd_runpy.py", line 127, in _run_module_code     _run_code(code, mod_globals, init_globals, mod_name, mod_spec, pkg_name, script_name)   File "/home/oscar/.vscode/extensions/ms-python.debugpy-2025.18.0-linux-x64/bundled/libs/debugpy/_vendored/pydevd/_pydevd_bundle/pydevd_runpy.py", line 118, in _run_code     exec(code, run_globals)   File "/home/oscar/py-projects/retro-rl-agents/src/retro_rl_agents/main.py", line 163, in <module>   File "/home/oscar/py-projects/retro-rl-agents/src/retro_rl_agents/main.py", line 79, in main
      File "/home/oscar/py-projects/retro-rl-agents/src/retro_rl_agents/services/call.py", line 46, in call_service
        mod.service(agent=agent, config=config)
      File "/home/oscar/py-projects/retro-rl-agents/src/retro_rl_agents/services/watch.py", line 22, in service
        obs, rew, term, trunc, info = env.step(action)
                                      ^^^^^^^^^^^^^^^^
      File "/home/oscar/py-projects/retro-rl-agents/.venv/lib/python3.12/site-packages/stable_retro/retro_env.py", line 217, in step
        for p, ap in enumerate(self.action_to_array(a)):
                               ^^^^^^^^^^^^^^^^^^^^^^^
      File "/home/oscar/py-projects/retro-rl-agents/.venv/lib/python3.12/site-packages/stable_retro/retro_env.py", line 204, in action_to_array
        action |= int(ap[i]) << i
                  ^^^^^^^^^^
    TypeError: only 0-dimensional arrays can be converted to Python scalars
    ```

---
+++ src/retro_rl_agents/services/watch.py
@@ -0,0 +1,25 @@
+import logging
+
+from stable_baselines3.common.base_class import BaseAlgorithm
+
+from retro_rl_agents.data_models.config_data import ConfigData
+
+NAME = __name__.split(".")[-1]
+logger = logging.getLogger(NAME)
+
+def service(agent: BaseAlgorithm, config: ConfigData) -> None:
+    """
+    Observe an agent's behaviour by watching it play the dang game.
+
+    Args:
+        agent (BaseAlgorithm): RL agent to train.
+        config (ConfigData): Config containing service params.
+    """
+    env = config.env    
+    obs, _ = env.reset()
+    while True:
+        action = agent.predict(obs, deterministic=config.deterministic)
+        obs, rew, term, trunc, info = env.step(action)
+        env.render()
+        if term or trunc:
+            break
