538f55a8642bfd824491a647b274c2680dd4aa8f
Oscar Sellors <omsellors98@gmail.com>
2026-05-04 19:36:37 +0100

    Migration file renaming

---
+++ db/migrations/001_create_eval_results.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS eval_results (
+    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
+    model_type          TEXT    NOT NULL,
+    model_settings      TEXT    NOT NULL,  -- store as YAML string
+    model_policy        TEXT,              -- store as YAML string
+    model_path          TEXT    NOT NULL,
+    env                 TEXT    NOT NULL,
+    env_settings        TEXT,              -- store as YAML string
+    avg_return          REAL,
+    std_return          REAL,
+    avg_ep_len          REAL,
+    std_ep_len          REAL,
+    full_results        TEXT,              -- store as YAML string
+    started_at          TEXT,
+    finished_at         TEXT,
+    config_settings     TEXT,              -- store as YAML string
+    sys_settings        TEXT
+);
---
+++ db/migrations/002_create_imitation_runs.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS imitation_runs (
+    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
+    model_type          TEXT    NOT NULL,
+    model_settings      TEXT    NOT NULL,  -- store as YAML string
+    model_policy        TEXT,              -- store as YAML string
+    model_path          TEXT,
+    save_path           TEXT    NOT NULL,
+    env                 TEXT    NOT NULL,
+    env_settings        TEXT,              -- store as YAML string
+    tb_path             TEXT,
+    imitation_type      TEXT    NOT NULL,
+    transitions_path    TEXT    NOT NULL,
+    imitation_settings  TEXT,              -- store as YAML string
+    avg_loss_final      REAL,
+    avg_ep_len_final    REAL,
+    started_at          TEXT,
+    finished_at         TEXT,
+    config_settings     TEXT,              -- store as YAML string
+    sys_settings        TEXT
+);
